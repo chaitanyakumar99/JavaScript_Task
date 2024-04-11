19594360d58344154e26281253489c2ed4133b3f
USER 1 <gchkumar999@gmail.com>
2024-04-11 14:17:01 +0530

    prat

--- Todo_App/script.js
+++ Todo_App/script.js
@@ -1,40 +1,40 @@
- const inputBox=document.getElementById("input-box");
- const listContainer=document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
- function addTask(){
-    if(inputBox.value === ''){
-      alert("You must write something!");
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
     }
-    else{
-        let li=document .createElement("li");
-        li.innerHTML=inputBox.value;
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        let span =document.createElement("span");
-        span.innerHTML="\u00d7";
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
-    } 
-    inputBox.value ="";
+    }
+    inputBox.value = "";
     saveData();
 }
 
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName === "LI"){
-       e.target.classList.toggle("checked");
-       saveData();
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
     }
-    else if(e.target.tagName === "SPAN"){
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
 }, false);
 
-function saveData(){
-       localStorage.setItem("data", listContainer.innerHTML); 
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
 // if we refrech the page to save the data we use getitem and to display items
 
-function showTask(){
-      listContainer.innerHTML=localStorage.getItem("data");
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
